54fc534f2b04b3c485af4be22b22392142b34fd7
Kendra McClanahan <kendramcclanahan@kendras-mbp.att.net>
2013-12-04 14:10:16 -0600

    Methods completed for adding, editing, and deleting location entries

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,29 +1,39 @@
 class LocationsController < ApplicationController
 
 	def show
-		
+		@location = Locations.find(params[:id])
 	end
 
 	def index
+		@locations = Locations.all
 	end
 
 	def new
+		@location = Locations.new
 	end
 
 	def create
-		
+		location = Locations.create(loca_params)
+		redirect_to root_path
 	end
 
 	def edit
-		
+		@location = Locations.find(params[:id])
 	end
 
 	def update
-		
+		@location = Locations.find(params[:id])
+		@location.update(loca_params)
+		redirect_to root_path
 	end
 
 	def destroy
+		@location.destroy
+	end
 
+	private
+	def loca_params
+		params.require(:location).permit(:picture_url, :name, :review, :address, :website)
 	end
 
 end
